b527ef7897c707894d07473b796f6eef12f6b570
craig <craig.stevenson@gmail.com>
2024-08-16 22:54:08 -0400

    updates

--- functions/index.js
+++ functions/index.js
@@ -26,38 +26,7 @@ exports.verifyAccount = require('./verifyAccount');
 
 exports.describeNetworkAcls = require('./describeNetworkAcls');
 
-exports.getRouteTables = functions.runWith({minInstances: 1}).https.onCall(async (data, context) => {
-  const email = context.auth.token.email;
-  const region = data.region;
-  if (!email) {
-    return {
-      "message": "no auth data",
-    };
-  }
-
-  const promise1 = admin.firestore().collection("keys").doc(email).get();
-  return promise1.then((result) => {
-    const _json = result.data();
-    const ec2 = new AWS.EC2({
-      apiVersion: "2016-11-15",
-      region: region,
-      credentials: {
-        accessKeyId: _json.awsKeyId,
-        secretAccessKey: _json.awsSecretAccessKey,
-      },
-    });
-
-    const ec2Promise = ec2.describeRouteTables().promise();
-    return ec2Promise.then((err, result) => {
-      if (err) {
-        return {
-          "message": err,
-        };
-      }
-      return result;
-    });
-  });
-});
+exports.getRouteTables = require('./getRouteTables');
 
 exports.getNatGateways = functions.runWith({minInstances: 1}).https.onCall((data, context) => {
   const email = context.auth.token.email;
